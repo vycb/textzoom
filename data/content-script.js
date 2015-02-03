ebe31c3715f2cc74d2637f7216c20f4209ef690c
vycb <vycb777@gmail.com>
2015-02-03 18:51:28 +0200

    initial

---
+++ data/content-script.js
@@ -0,0 +1,46 @@
+self.on("click", zoom);
+
+function zoom(node, data){
+	var args = data.split(','),
+		opval = args[0],
+		scale = args[1],
+		changeStep = args[2],
+		defFontSize = args[3],
+		op = opval === 'plus' ? 1: -1,
+		scale = opval === 'plus' ? scale: scale / 2
+		;
+
+	if(node instanceof HTMLImageElement)
+	{
+		if(!node.hasAttribute("width"))
+			node.setAttribute("width", node.naturalWidth);
+		if(!node.hasAttribute("height"))
+			node.setAttribute("height", node.naturalHeight);
+		if(!node.hasAttribute("originalWidth")){
+			node.setAttribute("originalWidth", node.width);
+			node.setAttribute("originalHeight", node.height);
+		}
+			node.width *= scale;
+			node.height *= scale;
+	}
+	else
+	{
+		if(node){
+			var
+				fontSize = parseInt(node.style.fontSize) + 0,	fontSize = !fontSize ? defFontSize : fontSize + changeStep * op
+				;
+
+			//node.parentNode.style.fontSize =
+			node.style.fontSize = (fontSize + "px");
+			//node.parentNode.style.lineHeight =
+			node.style.lineHeight = 'initial';
+		}
+	}
+
+	//logdata(node, data);
+}
+
+function logdata(node, data){
+	console.log("clicked: " + node.nodeName);
+	console.log("data: " + data);
+}
